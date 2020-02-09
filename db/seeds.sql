7330ee2d4dc81a2fe9414ccdaf73cbf9edf424a4
stuart lamble <lamble.stuart@googlemail.com>
2020-02-09 15:38:34 +0000

    Added Mysql schema & database seed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('You cheddar brielieve it burger');
+INSERT INTO burgers (name) VALUES ('The Sean Bean bean burger');
+INSERT INTO burgers (name) VALUES ('The Jean Luc Mustard burger');
